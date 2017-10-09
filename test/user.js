3eda7587c9332217ed0ca1096f2c0551f7bc3403
Jordan Cauley <jordan@cauley.co>
2017-10-09 14:37:22 -0400

    further refinements
    Still a bug that is preventing Mocha from exiting after tests run

--- test/user.js
+++ test/user.js
@@ -1,6 +1,6 @@
-process.env.NODE_ENV = 'test';
+process.env.NODE_ENV = 'test'
 
-//Require the dev-dependencies
+// Require the dev-dependencies
 let chai = require('chai')
 let chaiHttp = require('chai-http')
 let server = require('../index.js')
@@ -10,55 +10,45 @@ let faker = require('faker')
 const Models = require('../db.js')
 const User = Models.User
 
-chai.use(chaiHttp);
-//Our parent block
+chai.use(chaiHttp)
+// Our parent block
 describe('Users', () => {
-
   let username = faker.Internet.userName()
   let email = faker.Internet.email()
   let response = null
-
-  before((done) => { //Before each test we empty the database
-    return done()
-  });
 
   /*
   * Test the /POST route
   */
 
   describe('/POST user', () => {
-
     before((done) => {
       chai.request(server)
         .post('/api/v1/users')
         .send({
-          "username": username,
-          "email": email,
-          "password": "password123"
+          'username': username,
+          'email': email,
+          'password': 'password123'
         }).end((err, res) => {
           response = res
           return done()
         })
     })
-      
-    it('it should have STATUS 201', (done) => {
+
+    it('it should have STATUS 201', () => {
       response.should.have.status(201)
-      return done()
     })
 
-    it('it should CREATE a USER', (done) => {
+    it('it should CREATE a USER', () => {
       response.body.should.have.property('user')
       response.body.user.should.have.property('id')
       response.body.user.should.have.property('username')
       response.body.user.should.have.property('email')
-      return done()
     })
 
-    it('it should CREATE an AUTH', (done) => {
+    it('it should CREATE an AUTH', () => {
       response.body.should.have.property('user')
-      return done()
     })
-
   })
 
   /*
@@ -75,21 +65,18 @@ describe('Users', () => {
         })
     })
 
-    it('it should have STATUS 200', (done) => {
+    it('it should have STATUS 200', () => {
       response.should.have.status(200)
-      return done()
     })
 
-    it('it should GET all users', (done) => {
+    it('it should GET all users', () => {
       response.body.should.be.a('object')
       response.body.users.should.be.a('array')
       response.body.users.should.have.length(1)
-      return done()
     })
   })
 
-  after( function(done) {
-    console.log('Done')
+  after(function (done) {
     User.destroy({
       where: {},
       truncate: true
@@ -97,9 +84,8 @@ describe('Users', () => {
       return done()
     })
     .catch((err) => {
-      console.log(err)
       return done()
     })
-  }); 
+  })
 
 })
