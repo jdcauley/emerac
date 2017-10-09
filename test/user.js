35814136703b6eb38207bb39dcea7ea55b3f4a69
Jordan Cauley <jordan@cauley.co>
2017-10-09 11:01:47 -0400

    add faker in order to complete user creations
    Still working out DB connection issues for wiping existing data

--- test/user.js
+++ test/user.js
@@ -1,10 +1,11 @@
 process.env.NODE_ENV = 'test';
 
 //Require the dev-dependencies
-let chai = require('chai');
-let chaiHttp = require('chai-http');
-let server = require('../index.js');
-let should = chai.should();
+let chai = require('chai')
+let chaiHttp = require('chai-http')
+let server = require('../index.js')
+let should = chai.should()
+let faker = require('faker')
 
 const Models = require('../db.js')
 const User = Models.User
@@ -12,6 +13,9 @@ const User = Models.User
 chai.use(chaiHttp);
 //Our parent block
 describe('Users', () => {
+
+  let username = faker.Internet.userName()
+  let email = faker.Internet.email();
     // beforeEach((done) => { //Before each test we empty the database
     //     User.destroy({
     //       where: {},
@@ -27,29 +31,34 @@ describe('Users', () => {
             .get('/api/v1/users')
             .end((err, res) => {
                 res.should.have.status(200);
-                res.body.should.be.a('object');
-                res.body.users.should.be.a('array');
-                res.body.users.length.should.be.eql(0);
-              done();
-            });
-      });
-  });
+                res.body.should.be.a('object')
+                res.body.users.should.be.a('array')
+              done()
+            })
+      })
+  })
 
   describe('/POST user', () => {
       it('it should Create a USER', (done) => {
         chai.request(server)
             .post('/api/v1/users')
             .send({
-              "username": "testuser",
-              "email": "user@test.co",
+              "username": username,
+              "email": email,
               "password": "password123"
             })
             .end((err, res) => {
-                res.should.have.status(201);
-                res.body.should.be.a('object');
+                chai.expect(res).to.be.json
+                res.should.have.status(201)
+                res.body.should.be.a('object')
+                res.body.should.have.property('auth')
+                res.body.should.have.property('user')
+                res.body.user.should.have.property('id')
+                res.body.user.should.have.property('username')
+                res.body.user.should.have.property('email')
               done();
-            });
-      });
-  });
+            })
+      })
+  })
 
-});
+})
