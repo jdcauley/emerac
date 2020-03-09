2d9261c42111100c2969a163a9846922f6aabbbf
Jordan Cauley <jordan@cauley.co>
2020-03-08 20:31:39 -0400

    Update all critical dependencies

--- src/config/boot.js
+++ src/config/boot.js
@@ -1,10 +1,10 @@
-const fs = require('fs')
-const path = require('path')
-const express = require('express')
-const session = require('express-session')
-const bodyParser = require('body-parser')
-const routes = require('./routes.js')
-const secret = process.env.JWT_SECRET || 'use-this-or-gen-new-secret'
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const routes = require("./routes.js");
+const secret = process.env.JWT_SECRET || "use-this-or-gen-new-secret";
 const sessionConfig = {
   secret,
   proxy: true,
@@ -13,70 +13,71 @@ const sessionConfig = {
   cookie: {
     maxAge: 3600000
   }
-}
-if (process.env.NODE_ENV === 'production') {
-  sessionConfig.cookie.secure = true
+};
+if (process.env.NODE_ENV === "production") {
+  sessionConfig.cookie.secure = true;
 }
 // fetch controllers
-const Models = require('../db.js')
+const Models = require("../db.js");
 
 module.exports = async () => {
+  let app = express();
 
-  let app = express()
+  app.set("view engine", "ejs");
+  app.set("trust proxy", 1);
 
-  app.set('view engine', 'ejs')
-  app.set('trust proxy', 1)
-
-  app.use('/assets', express.static(path.resolve('assets')))
-  app.use(session(sessionConfig))
-  app.use(bodyParser.json())
-  app.use(bodyParser.urlencoded({ extended: true }))
-  app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-    next()
-  })
+  app.use("/assets", express.static(path.resolve("assets")));
+  app.use(session(sessionConfig));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    next();
+  });
   app.use((req, res, next) => {
     // if there's a flash message in the session request, make it available in the response, then delete it
     if (req.session && req.session.flash) {
-      req.session.flash = []
+      req.session.flash = [];
     }
     next();
-  })
+  });
 
   try {
-    const Controllers = {}
-    const Services = {}
+    const Controllers = {};
+    const Services = {};
 
-    let controllerPath = path.join(__dirname, '../api/controllers')
-    let servicesPath = path.join(__dirname, '../api/services')
+    let controllerPath = path.join(__dirname, "../api/controllers");
+    let servicesPath = path.join(__dirname, "../api/services");
 
-    fs.readdirSync(controllerPath).forEach((file) => {
-      var fileName = file.split('.')
-      if (fileName[0] === 'tests') {
-        return
+    fs.readdirSync(controllerPath).forEach(file => {
+      var fileName = file.split(".");
+      if (fileName[0] === "tests") {
+        return;
       }
-      Controllers[fileName[0]] = require('../api/controllers/' + file)
-    })
+      Controllers[fileName[0]] = require("../api/controllers/" + file);
+    });
 
-    fs.readdirSync(servicesPath).forEach((file) => {
-      var fileName = file.split('.')
-      if (fileName[0] === 'tests') {
-        return
+    fs.readdirSync(servicesPath).forEach(file => {
+      var fileName = file.split(".");
+      if (fileName[0] === "tests") {
+        return;
       }
-      Services[fileName[0]] = require('../api/services/' + file)
-    })
+      Services[fileName[0]] = require("../api/services/" + file);
+    });
 
-    app.models = await Models()
-    app.services = Services
-    app.controllers = Controllers
+    app.models = await Models();
+    app.services = Services;
+    app.controllers = Controllers;
 
-    app.apiPrefix = '/api/v1'
-    app = routes(app)
+    app.apiPrefix = "/api/v1";
+    app = routes(app);
 
-    return app
-  } catch(err) {
-    throw err
+    return app;
+  } catch (err) {
+    throw err;
   }
-
-}
+};
--- src/config/routes.js
+++ src/config/routes.js
@@ -1,41 +1,66 @@
-const expressJwt = require('express-jwt')
-let JWT_SECRET = process.env.JWT_SECRET || 'use-this-or-gen-new-secret'
+const expressJwt = require("express-jwt");
+let JWT_SECRET = process.env.JWT_SECRET || "use-this-or-gen-new-secret";
 
-module.exports = (app) => {
-
-  const apiPrefix = app.apiPrefix
+module.exports = app => {
+  const apiPrefix = app.apiPrefix;
 
   // User Routes
-  app.post(`${apiPrefix}/users`, app.controllers.user.create)
-  app.get(apiPrefix + '/users', expressJwt({secret: JWT_SECRET, credentialsRequired: false}), app.controllers.user.find)
-  app.get(apiPrefix + '/users/:id', expressJwt({secret: JWT_SECRET}), app.controllers.user.findById)
-  app.patch(apiPrefix + '/users', expressJwt({secret: JWT_SECRET}), app.controllers.user.update)
-  app.put(apiPrefix + '/users', expressJwt({secret: JWT_SECRET}), app.controllers.user.update)
-  app.delete(apiPrefix + '/users', expressJwt({secret: JWT_SECRET}), app.controllers.user.destroy)
+  app.post(`${apiPrefix}/users`, app.controllers.user.create);
+  app.get(
+    apiPrefix + "/users",
+    expressJwt({ secret: JWT_SECRET, credentialsRequired: false }),
+    app.controllers.user.find
+  );
+  app.get(
+    apiPrefix + "/users/:id",
+    expressJwt({ secret: JWT_SECRET }),
+    app.controllers.user.findById
+  );
+  app.patch(
+    apiPrefix + "/users",
+    expressJwt({ secret: JWT_SECRET }),
+    app.controllers.user.update
+  );
+  app.put(
+    apiPrefix + "/users",
+    expressJwt({ secret: JWT_SECRET }),
+    app.controllers.user.update
+  );
+  app.delete(
+    apiPrefix + "/users",
+    expressJwt({ secret: JWT_SECRET }),
+    app.controllers.user.destroy
+  );
 
   // Auth Routes
-  app.post(apiPrefix + '/auth', app.controllers.auth.login)
-  app.post(apiPrefix + '/auth/password/reset/start', app.controllers.auth.startPasswordReset)
-  app.get('/password/reset/:token', app.controllers.auth.renderForm)
-  app.post(apiPrefix + '/auth/password/reset/', app.controllers.auth.savePasswordReset)
+  app.post(apiPrefix + "/auth", app.controllers.auth.login);
+  app.post(
+    apiPrefix + "/auth/password/reset/start",
+    app.controllers.auth.startPasswordReset
+  );
+  app.get("/password/reset/:token", app.controllers.auth.renderForm);
+  app.post(
+    apiPrefix + "/auth/password/reset/",
+    app.controllers.auth.savePasswordReset
+  );
 
-  app.get(apiPrefix + '/routes', (req, res) => {
+  app.get(apiPrefix + "/routes", (req, res) => {
     return res.status(200).json({
       endpoints: app._router.stack.filter(r => r.route).map(r => r.route.path)
-    })
-  })
-  
-  app.get(apiPrefix + '/status', (req, res) => {
+    });
+  });
+
+  app.get(apiPrefix + "/status", (req, res) => {
     return res.status(200).json({
-      apiStatus: 'ok'
-    })
-  })
+      apiStatus: "ok"
+    });
+  });
 
-  app.get('*', (req, res) => {
-    return res.status(404).render('404', {
+  app.get("*", (req, res) => {
+    return res.status(404).render("404", {
       session: req.session
-    })
-  })
+    });
+  });
 
-  return app
-}
+  return app;
+};
